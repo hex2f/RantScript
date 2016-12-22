a68201b7446c953ed907c66b9d13ed7de527c96c
RekkyRek <haslun16@edu.vilhelmina.se>
2016-12-22 21:57:00 +0100

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,14 +1,1 @@
-var devrant = require('./src/index.js');
-devrant
-  .login('Dacexi', '0hg0d1l0v3m3m3s')
-  .then((response)=>{
-    devrant.vote(
-      1,
-      272008,
-      response["auth_token"]["id"],
-      response["auth_token"]["key"],
-      response["auth_token"]["user_id"]
-    ).then((resp)=>{
-      console.log(resp);
-    })
-  });
+
