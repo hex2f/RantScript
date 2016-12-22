22326c5abe4163f8db31d25065645ac551c06c0d
RekkyRek <haslun16@edu.vilhelmina.se>
2016-12-22 21:56:48 +0100

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,18 +1,4 @@
 var devrant = require('./src/index.js');
-/*devrant
-  .login('Dacexi', '0hg0d1l0v3m3m3s')
-  .then((response)=>{
-    devrant.postComment(
-      "This was posted from my devRant api wrapper.",
-      342726,
-      response["auth_token"]["id"],
-      response["auth_token"]["key"],
-      response["auth_token"]["user_id"]
-    ).then((resp)=>{
-      console.log(resp);
-    })
-  })
-*/
 devrant
   .login('Dacexi', '0hg0d1l0v3m3m3s')
   .then((response)=>{
