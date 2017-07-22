2ee607981ee3212bfdfc23bc41c4c35bfe00618e
hampus <haslun16@edu.vilhelmina.se>
2017-07-22 20:37:26 +0200

    Removed timeout

--- testscript.js
+++ testscript.js
@@ -6,14 +6,10 @@ console.log('Debug: ', devrant.httpSettings.GET_DEBUG());
 console.log('Compress: ', devrant.httpSettings.GET_COMPRESS());
 var auth = {
   id: 730130,
-  key: 'WY38fmK1J8kGqahihCqPk_EFGyLuz8RA_9enwcss',
+  key: 'HIDDEN',
   expire_time: 1503340260,
   user_id: 161184
 }
 
-console.log(auth["auth_token"])
-
-setTimeout(()=>{
-  devrant
-    .postRant("Test Body", "Some, Tags, Here", auth, __dirname+'/images/RantScript.png')
-}, 1000)
+devrant
+  .postRant("Test Body", "Some, Tags, Here", auth, __dirname+'/images/RantScript.png')
